3cb217c15e2857106f07af54d3fa88e100899f71
dwhddnjs <per1215@naver.com>
2021-08-01 23:25:37 +0900

    add login submit button

--- vue-form/src/App.vue
+++ vue-form/src/App.vue
@@ -1,22 +1,33 @@
 <template>
   <div>
-    <form>
+    <form v-on:submit='submitForm'>
       <div>
         <label for="username">ID: </label>
-        <input id='username' type="text">
+        <input id='username' type="text" v-model='username'>
       </div>
       <div>
         <label for="password">PW: </label>
-        <input id='password' type="text">
+        <input id='password' type="password" v-model='password'>
       </div>
-      <button>로그인</button>
+      <button type='submit'>로그인</button>
     </form>
   </div>
 </template>
 
 <script>
 export default {
-
+  data() {
+    return {
+      username:'',
+      password:''
+    }
+  },
+  methods: {
+    submitForm(e) {
+      e.preventDefault()
+      console.log('submitted')
+    }
+  }  
 }
 </script>
 
