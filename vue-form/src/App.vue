a4d1ac48cdcbf96948aa6fb86db7057e1dae412e
dwhddnjs <per1215@naver.com>
2021-08-02 02:15:59 +0900

    add emailvaildation

--- vue-form/src/App.vue
+++ vue-form/src/App.vue
@@ -9,13 +9,20 @@
         <label for="password">PW: </label>
         <input id='password' type="password" v-model='password'>
       </div>
-      <button type='submit'>로그인</button>
+      <button v-bind:disabled='!isUsernameVaild' type='submit'>로그인</button>
     </form>
     <p v-if='isError'>올바르지 않은 ID입니다</p>
+    <p v-if="isUsernameVaild">이메일 형식이 맞습니다</p>
   </div>
 </template>
 
 <script>
+// 이메일 형식 체크 함수
+function validateEmail(email) {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
+
 export default {
   data() {
     return {
@@ -23,7 +30,11 @@ export default {
       password:'',
       isError:false
     }
-
+  },
+  computed: {
+    isUsernameVaild() {
+      return validateEmail(this.username)
+    }
   },
   methods: {
     submitForm() {
